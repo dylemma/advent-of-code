8acc106d87126956b72c911dbb151c185820c6bf
Dylan Halperin <dylan@dylemma.io>
2022-12-01 07:09:15 -0500

    set custom code style

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,9 @@
 package example
 
 object Hello extends Greeting with App {
-  println(greeting)
+	println(greeting)
 }
 
 trait Greeting {
-  lazy val greeting: String = "hello"
+	lazy val greeting: String = "hello"
 }
