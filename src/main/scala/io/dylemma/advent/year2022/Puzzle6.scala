9cca999441d93fea7ec2d63ba1f60afd1cf3964a
Dylan Halperin <dylan@dylemma.io>
2022-12-06 07:54:19 -0500

    solve puzzle 6

---
+++ src/main/scala/io/dylemma/advent/year2022/Puzzle6.scala
@@ -0,0 +1,19 @@
+package io.dylemma.advent.year2022
+
+import io.dylemma.advent.util.Puzzle
+
+object Puzzle6 extends Puzzle {
+	def findStartMarker(size: Int, chars: Iterator[Char]) = chars
+		.sliding(size)
+		.indexWhere { _.toSet.sizeCompare(size) == 0 }
+		.+(size) // my commitment to chained method call style is unwavering
+
+	def part1(): String = {
+		findStartMarker(4, inputLines.head.iterator).toString
+
+		
+	}
+	def part2(): String = {
+		findStartMarker(14, inputLines.head.iterator).toString
+	}
+}
