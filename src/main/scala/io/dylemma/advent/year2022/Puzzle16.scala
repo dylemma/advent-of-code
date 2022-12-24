58e90245e192ed5202c32448d9b96b8f8a6caef7
Dylan Halperin <dylan@dylemma.io>
2022-12-23 19:54:32 -0500

    Slightly improve puzzle16 perf

--- src/main/scala/io/dylemma/advent/year2022/Puzzle16.scala
+++ src/main/scala/io/dylemma/advent/year2022/Puzzle16.scala
@@ -3,38 +3,44 @@ package year2022
 
 import io.dylemma.advent.util.Matching._
 
-import scala.util.chaining._
 import scala.annotation.tailrec
 import scala.collection.immutable.Queue
-import scala.collection.mutable
-
-import cats.data.WriterT
 
 object Puzzle16 extends util.Puzzle {
-	case class ValveId(id: String) extends AnyVal {
-		override def toString = id
+	case class ValveId(index: Int) extends AnyVal {
+		override def toString = valves(index).label
 	}
-	case class Valve(id: ValveId, flowRate: Int, tunnelDestinations: List[ValveId])
-	val ValveLine = raw"Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? (.*)".r
-	val valves = inputLines
-		.view
-		.map { case ValveLine(id, AsInt(rate), rawDests) => Valve(ValveId(id), rate, rawDests.split(", ").view.map(_.trim).map(ValveId).toList) }
-		.map { v => v.id -> v }
-		.toMap
+	case class Valve(id: ValveId, label: String, flowRate: Int, tunnelDestinations: List[ValveId])
+
+	val valves = {
+		val ValveLine = raw"Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? (.*)".r
+		val parsed = inputLines
+			.view
+			.zipWithIndex
+			.map { case (ValveLine(label, AsInt(rate), rawDests), idx) => (idx, label, rate, rawDests) }
+			.toVector
+		val idForLabel = parsed.view.map { case (idx, label, _, _) => label -> ValveId(idx) }.toMap
+		parsed.map { case (idx, label, rate, rawDests) =>
+			val dests = rawDests.split(", ").view.map(_.trim).map(idForLabel).toList
+			Valve(ValveId(idx), label, rate, dests)
+		}
+	}
+
+	val valveAA = valves.find(_.label == "AA").get.id
 
 	def bfs(from: ValveId, to: ValveId) = {
 		@tailrec def search(q: Queue[(ValveId, Int)], visited: Set[ValveId]): Int = {
 			val ((current, depth), tail) = q.dequeue
 			if (visited(current)) search(tail, visited)
 			else if (current == to) depth
-			else search(tail.enqueueAll(valves(current).tunnelDestinations.view.map(_ -> (depth + 1))), visited + current)
+			else search(tail.enqueueAll(valves(current.index).tunnelDestinations.view.map(_ -> (depth + 1))), visited + current)
 		}
 
 		search(Queue(from -> 0), Set.empty)
 	}
 
-	val usefulValves = valves.valuesIterator.filter(_.flowRate > 0).map(_.id).toSet
-	val pathfindingValves = usefulValves + ValveId("AA")
+	val usefulValves = valves.view.filter(_.flowRate > 0).map(_.id).toSet
+	val pathfindingValves = usefulValves + valveAA
 
 	// from any given "pathfinding" valve, precompute the distance to each "useful" valve
 	val distances = pathfindingValves.view.map { from =>
@@ -45,48 +51,38 @@ object Puzzle16 extends util.Puzzle {
 	case class Open(id: ValveId, time: Int) extends Step
 	case class Wait(time: Int) extends Step
 
-	case class Path(steps: List[Step]) {
-		def toDebugString = steps.mkString("[", " -> ", "]")
-		def simulateRateIncreases: Iterator[Int] = Iterator.unfold(steps) {
-			case Open(id, 1) :: tail => Some(valves(id).flowRate -> tail)
-			case Open(id, t) :: tail => Some(0 -> (Open(id, t - 1) :: tail))
-			case Wait(1) :: tail => Some(0 -> tail)
-			case Wait(t) :: tail => Some(0 -> (Wait(t - 1) :: tail))
-			case Nil => None
-		}
-	}
-
-	def simulate(actorPaths: List[Path]) = {
-		val rateIncreases = actorPaths.view.map(_.simulateRateIncreases).reduce(_.zip(_).map { case (a, b) => a + b })
-		rateIncreases.foldLeft(0 -> 0) { case ((rate, accum), inc) =>
-			(rate + inc, accum + rate)
-		}._2
-	}
-
 	case class ActorState(
 		currentMinute: Int, // start at 0
 		pos: ValveId,
 		closedValves: Set[ValveId],
+		rate: Int,
+		accum: Int,
 		pathRev: List[Step],
 	) {
-		def path = Path(pathRev.reverse)
 		// new state if the actor travels to the target valve then spends a minute opening it
 		def open(target: ValveId) = {
-			val travelTime =
-				if (target == pos) 0
-				else distances(pos)(target)
+			val timeSpent = // travel time plus a minute to open the valve
+				if (target == pos) 1
+				else distances(pos)(target) + 1
 			ActorState(
-				currentMinute + travelTime + 1,
+				currentMinute + timeSpent,
 				target,
 				closedValves - target,
-				Open(target, travelTime + 1) :: pathRev,
+				rate + valves(target.index).flowRate,
+				accum + (rate * timeSpent),
+				Open(target, timeSpent) :: pathRev,
 			)
 		}
 		// new state if the actor just sits around until the given minute
 		def waitUntil(minute: Int) = {
-			this.copy(
+			val waitTime = minute - currentMinute
+			ActorState(
 				currentMinute = minute,
-				pathRev = Wait(minute - currentMinute) :: pathRev,
+				pos,
+				closedValves,
+				rate,
+				accum + (rate * waitTime),
+				pathRev = Wait(waitTime) :: pathRev,
 			)
 		}
 
@@ -98,7 +94,6 @@ object Puzzle16 extends util.Puzzle {
 
 		def toDebugString = s"[${ pathRev.reverse.mkString(" -> ") }], time=${ currentMinute }"
 	}
-
 
 	def dfs(current: ActorState, timeLimit: Int): Iterator[ActorState] = current.nextPossibleStates(timeLimit) match {
 		case Nil =>
@@ -117,57 +112,62 @@ object Puzzle16 extends util.Puzzle {
 
 		val init = ActorState(
 			currentMinute = 0,
-			pos = ValveId("AA"),
+			pos = valveAA,
 			closedValves = usefulValves,
+			rate = 0,
+			accum = 0,
 			pathRev = Nil,
 		)
 
 		var count = 0
-		val (bestPath, bestValue) = dfs(init, 30)
-			.map { s => s -> simulate(s.path :: Nil) }
-//			.tapEach { case (s, v) => println(s"search @ ${s.toDebugString}, value=$v")}
-			.tapEach { _ => count += 1}
-			.maxBy(_._2)
+		val bestPath = dfs(init, 30)
+			.tapEach { _ => count += 1 }
+			.maxBy(_.accum)
 		println(s"Explored $count possible paths")
-		println(s"Best path: ${bestPath.toDebugString}\nBest value: $bestValue")
-		bestValue.toString
+		println(s"Best path: ${ bestPath.toDebugString }")
+		bestPath.accum.toString
 	}
 	def part2(): String = {
 		val init = ActorState(
 			currentMinute = 0,
-			pos = ValveId("AA"),
+			pos = valveAA,
 			closedValves = usefulValves,
+			rate = 0,
+			accum = 0,
 			pathRev = Nil,
 		)
 
-		val pathPairs = for {
+		val statePairs = for {
 			myPath <- dfs(init, 26)
 			eleInit = ActorState(
 				currentMinute = 0,
-				pos = ValveId("AA"),
+				pos = valveAA,
 				myPath.closedValves,
+				rate = 0,
+				accum = 0,
 				pathRev = Nil,
 			)
 			elePath <- dfs(eleInit, 26)
-		} yield myPath.path :: elePath.path :: Nil
+		} yield myPath :: elePath :: Nil
 
 		var count = 0
-		val (bestPair, bestValue) = pathPairs
-			.map { pair => pair -> simulate(pair) }
-//			.tapEach { _ =>
-//				count += 1
-//				if (count % 1000000 == 1) println(s"searching... ${count - 1}")
-//			}
+		val (bestPair, bestValue) = statePairs
+			.map { pair => pair -> pair.view.map(_.accum).sum }
+			.tapEach { _ =>
+				count += 1
+				if (count % 1000000 == 1) println(s"searching... ${ count - 1 }")
+			}
 			.maxBy(_._2)
 		println(s"Explored $count possible paths")
 		println("Best paths:")
-		for (p <- bestPair) println(s"  ${p.toDebugString}")
+		for (p <- bestPair) println(s"  ${ p.toDebugString }")
 		println(s"  value: $bestValue")
 
 		// Best paths:
 		//  [Open(IK,3) -> Open(VK,3) -> Open(IT,3) -> Open(WH,3) -> Open(WL,3) -> Open(HG,4) -> Wait(7)]
 		//  [Open(EZ,3) -> Open(BV,3) -> Open(CF,3) -> Open(JX,4) -> Open(LB,3) -> Open(WM,4) -> Wait(6)]
 		//  value: 2469
+		// [success] Total time: 578 s (09:38),
 		bestValue.toString
 	}
 }
