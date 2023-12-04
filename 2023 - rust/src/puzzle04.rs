72257e3014591a6a65ef01ec1a73920e956642df
Dylan Halperin <dylan@dylemma.io>
2023-12-04 18:25:03 -0500

    solve puzzle 4

--- 2023 - rust/src/puzzle04.rs
+++ 2023 - rust/src/puzzle04.rs
@@ -8,23 +8,44 @@ use crate::helper::GenResult;
 
 pub fn run(path: &Path) -> GenResult<()> {
 
-    let sum: u32 = BufReader::new(File::open(path)?).lines()
-        .map(|r| r.map_err(|e| e.to_string()))
+    // Parse lines of input as `Card`s, collecting them into a `Vec`
+    let cards: Vec<Card> = BufReader::new(File::open(path)?)
+        .lines()
         .map(|line| {
-            let card = Card::try_from(line?.as_str())?;
-            let score = card.score();
-            println!("{:?} -> {}", card, score);
-            Ok(score)
+            let line = line.map_err(|e| e.to_string())?;
+            let card = Card::try_from(line.as_str())?;
+            println!("{} -> num_matches: {}", line, card.num_matches());
+            Ok(card)
         })
-        .sum::<Result<u32, String>>()?;
+        .collect::<Result<_, String>>()?;
 
-    println!("sum: {}", sum);
+    // For Part 1, compute the sum of each card's "score"
+    let score_sum: u32 = cards.iter().map(Card::score).sum();
+
+    // For Part 2, accumulate a number of cards, where each card
+    // may influence the count of some number of cards that appear
+    // after it.
+    let num_cards: u32 = {
+        let mut card_counts = vec![1u32; cards.len()];
+        for (index, card) in cards.iter().enumerate() {
+            let num_matches = card.num_matches();
+            let num_copies = card_counts[index];
+            for dx in 1..=num_matches {
+                card_counts[index + dx as usize] += num_copies;
+            }
+        }
+        card_counts.iter().sum()
+    };
+
+    println!("Score Sum: {}", score_sum);
+    println!("Num Cards: {}", num_cards);
     Ok(())
 }
 
 /// Interpretation of a line of puzzle input
 #[derive(Debug)]
 struct Card {
+    #[allow(unused)] // even though it didn't turn out to be relevant, it's still nice
     id: u32,
     winning_numbers: HashSet<u32>,
     my_numbers: Vec<u32>,
@@ -33,17 +54,21 @@ struct Card {
 impl Card {
     /// Scoring function for puzzle Part 1
     fn score(&self) -> u32 {
-        let num_matches = self
-            .my_numbers
-            .iter()
-            .filter(|n| self.winning_numbers.contains(n))
-            .count();
+        let num_matches = self.num_matches();
 
         if num_matches > 0 {
             2u32.pow(num_matches as u32 - 1)
         } else {
             0
         }
+    }
+
+    fn num_matches(&self) -> u32 {
+        self
+            .my_numbers
+            .iter()
+            .filter(|n| self.winning_numbers.contains(n))
+            .count() as u32
     }
 }
 
