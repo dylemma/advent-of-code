5fa7ecd624301cd0fc2d0eb6690884749c0f772d
Dylan Halperin <dylan@dylemma.io>
2024-12-11 21:30:43 -0500

    solve puzzle 7 part 2

--- 2024 - rust/src/puzzle07.rs
+++ 2024 - rust/src/puzzle07.rs
@@ -1,4 +1,5 @@
 use crate::helper::GenResult;
+use colored::Colorize;
 use log::info;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter};
@@ -6,7 +7,6 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 use std::str::FromStr;
-use colored::Colorize;
 
 pub fn run(input_path: &Path) -> GenResult<()> {
     let puzzle_lines: Vec<PuzzleLine> = {
@@ -18,28 +18,27 @@ pub fn run(input_path: &Path) -> GenResult<()> {
             .collect::<GenResult<Vec<_>>>()?
     };
 
-    let mut sum_of_solved = 0u64;
+    let mut part1_sum = 0u64;
+    let mut part2_sum = 0u64;
+
     for line in puzzle_lines {
         let solutions = line.solve();
-        if solutions.is_empty() {
-            info!("{}", Unsolved(line))
-        } else {
-            let first = &solutions[0];
-            sum_of_solved += first.output;
-            let second = &solutions[1..];
-            if second.is_empty() {
-                info!("{}", first);
-            } else {
-                info!("{} (and {} alternate solutions)", first, second.len());
+        if solutions.is_solved() {
+            part2_sum += solutions.puzzle.output;
+            if !solutions.required_concat {
+                part1_sum += solutions.puzzle.output;
             }
         }
+        info!("{}", solutions);
     }
 
-    info!("Sum of solved: {}", sum_of_solved.to_string().green());
+    info!("Part 1 sum: {}", part1_sum.to_string().green());
+    info!("Part 2 sum: {}", part2_sum.to_string().yellow());
 
     Ok(())
 }
 
+/// Represents a line from the day 7 puzzle input
 #[derive(Debug)]
 struct PuzzleLine {
     output: u64,
@@ -61,47 +60,88 @@ impl FromStr for PuzzleLine {
 }
 
 impl PuzzleLine {
-    fn solve(&self) -> Vec<Solution> {
+    fn solve(self) -> Solutions {
         if self.inputs.is_empty() {
-            Vec::new()
+            Solutions {
+                puzzle: self,
+                solutions: Vec::new(),
+                required_concat: false,
+            }
         } else {
-            let mut out = Vec::new();
-            recursive_solve(self.output, (self.inputs[0], Vec::new()), &self.inputs[1..], &mut out);
-            out.into_iter().map(|operators| {
-                Solution {
-                    inputs: self.inputs.clone(),
-                    operators,
-                    output: self.output,
-                }
-            }).collect()
+            let mut required_concat = false;
+            let mut solutions = (&self).inner_solve(&[Operator::Plus, Operator::Times]);
+            if solutions.is_empty() {
+                required_concat = true;
+                solutions =
+                    (&self).inner_solve(&[Operator::Plus, Operator::Times, Operator::Concat]);
+            }
+            Solutions {
+                puzzle: self,
+                solutions,
+                required_concat,
+            }
+        }
+    }
+
+    fn inner_solve(&self, operators: &[Operator]) -> Vec<Solution> {
+        let mut out = Vec::new();
+        Self::recursive_solve(
+            self.output,
+            (self.inputs[0], Vec::new()),
+            &self.inputs[1..],
+            operators,
+            &mut out,
+        );
+        out.into_iter()
+            .map(|operators| Solution {
+                inputs: self.inputs.clone(),
+                operators,
+                output: self.output,
+            })
+            .collect()
+    }
+
+    // Basically a DFS through a binary/trinary tree;
+    // Each step inspects the next number from the `tail`, once per operator, resulting in
+    // O(num_numbers ^ num_operators) worst case performance.
+    // Includes an early-exit to avoid continuing down a search path whose `accum`
+    // total already went past the `goal` number.
+    fn recursive_solve(
+        goal: u64,
+        accum: (u64, Vec<Operator>),
+        tail: &[u64],
+        operators: &[Operator],
+        out: &mut Vec<Vec<Operator>>,
+    ) {
+        let (total, path) = accum;
+        if total > goal {
+            return; // since there are no decreasing operators, use this to exit early
+        }
+        if tail.is_empty() {
+            // recursion end case
+            if total == goal {
+                out.push(path)
+            }
+        } else {
+            let head = tail[0];
+            let remaining = &tail[1..];
+
+            for op in operators {
+                let next_total = op.apply(total, head);
+                let mut next_path = path.clone();
+                next_path.push(*op);
+                Self::recursive_solve(goal, (next_total, next_path), remaining, operators, out);
+            }
         }
     }
 }
 
-fn recursive_solve(goal: u64, accum: (u64, Vec<Operator>), tail: &[u64], out: &mut Vec<Vec<Operator>>) {
-    let (total, path) = accum;
-    if tail.is_empty() {
-        // recursion end case
-        if total == goal {
-            out.push(path)
-        }
-    } else {
-        let head = tail[0];
-        let remaining = &tail[1..];
-
-        for op in [Operator::Plus, Operator::Times] {
-            let next_total = op.apply(total, head);
-            let mut next_path = path.clone();
-            next_path.push(op);
-            recursive_solve(goal, (next_total, next_path), remaining, out);
-        }
-    }
-}
-
+/// Enum of the operators defined for day 7
 #[derive(Copy, Clone)]
 enum Operator {
     Plus,
     Times,
+    Concat,
 }
 
 impl Operator {
@@ -109,29 +149,34 @@ impl Operator {
         match self {
             Operator::Plus => a + b,
             Operator::Times => a * b,
+            Operator::Concat => format!("{}{}", a, b).parse().unwrap(),
         }
     }
 }
 
+// Fancy colored output for operators
 impl Display for Operator {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let str = match self {
             Operator::Plus => "+".to_string().cyan(),
             Operator::Times => "*".to_string().bright_magenta(),
+            Operator::Concat => "||".to_string().yellow(),
         };
         write!(f, "{}", str)
     }
 }
 
+// One possible solution to a puzzle
 struct Solution {
     inputs: Vec<u64>,
     operators: Vec<Operator>,
     output: u64,
 }
 
+// Fancy colored + formatted output for a puzzle solution
 impl Display for Solution {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} = ", self.output.to_string().green());
+        write!(f, "{} = ", self.output.to_string().green())?;
         for (i, n) in self.inputs.iter().enumerate() {
             if i > 0 {
                 write!(f, " {} ", self.operators[i - 1].to_string().yellow())?;
@@ -142,9 +187,43 @@ impl Display for Solution {
     }
 }
 
-struct Unsolved(PuzzleLine);
+// Aggregation of solutions to a puzzle
+struct Solutions {
+    puzzle: PuzzleLine,
+    solutions: Vec<Solution>,
+    required_concat: bool,
+}
 
-impl Display for Unsolved {
+impl Solutions {
+    fn is_solved(&self) -> bool {
+        !self.solutions.is_empty()
+    }
+}
+
+// Nice output for the solutions
+impl Display for Solutions {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        if self.solutions.is_empty() {
+            write!(f, "   {}", Unsolved(&self.puzzle))
+        } else {
+            if self.required_concat {
+                write!(f, " {} ", "^".yellow())?;
+            } else {
+                write!(f, "{}{} ", "^".green(), "^".yellow())?;
+            }
+            write!(f, "{}", &self.solutions[0])?;
+            if self.solutions.len() > 1 {
+                write!(f, " (and {} alternate solutions)", self.solutions.len() - 1)?;
+            }
+            Ok(())
+        }
+    }
+}
+
+struct Unsolved<'a>(&'a PuzzleLine);
+
+// Nice output for an unsolved puzzle
+impl<'a> Display for Unsolved<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{} = ", self.0.output.to_string().red())?;
         for (i, n) in self.0.inputs.iter().enumerate() {
